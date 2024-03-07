80864065c481278a39b6a0dd5926be920a32835d
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-07 23:36:09 +0530

    minute changes

--- language_stats.R
+++ language_stats.R
@@ -16,12 +16,13 @@ data_long$Year <- as.numeric(format(as.yearmon(data_long$Date, "%B %Y"), "%Y"))
 
 # Define ggplot object
 scatter <- ggplot(data_long, aes(x = programming_language, y = popularity, color = Year)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_point(showSelected = "Year") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x = "Programming Language", y = "Popularity", title = "Popularity of Programming Languages Over Time")
+scatter 
 
 # Define animint object with specified duration
-viz.duration <- animint(scatter, duration = list(Year=2000))
+viz.duration <- animint(scatter, duration = list(Year=2000),out.dir = "./output")
 viz.duration.time <- viz.duration
-viz.duration.time$time <- list(variable="Year", ms=2000)
+viz.duration.time$time <- list(variable="Year", ms=3000)
 viz.duration.time
